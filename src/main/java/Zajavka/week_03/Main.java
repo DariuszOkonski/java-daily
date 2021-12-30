7033b643fd6e9bd7638c5c52b169e808dcc5de19
Dariusz Okonski <darek200180@gmail.com>
2021-12-30 19:30:34 +0000

    add package 10

--- src/main/java/Zajavka/week_03/Main.java
+++ src/main/java/Zajavka/week_03/Main.java
@@ -4,13 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s1 = "hello";
-        String s2 = "hello";
-        String s3 = "test";
 
 
-        System.out.println(s1.equals(s2));
-        System.out.println(s1 == s2);
 
     }
 }
---
+++ src/main/java/Zajavka/week_03/_10/Main.java
@@ -0,0 +1,32 @@
+package Zajavka.week_03._10;
+
+public class Main {
+    public static void main(String[] args) {
+        externalMethod(method1() + method2(method3() + method4()));
+    }
+
+    private static int method2(int number) {
+        System.out.println("method2");
+        return 2;
+    }
+
+    private static int method4() {
+        System.out.println("method4");
+        return 4;
+    }
+
+    private static int method3() {
+        System.out.println("method3");
+        return 3;
+    }
+
+    private static int method1() {
+        System.out.println("method1");
+        return 1;
+    }
+
+    public static void externalMethod(int arg) {
+        System.out.println("externalMethod");
+    }
+
+}
