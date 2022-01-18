584c29ac852914124bdc67fdf3e288450acf466d
Dariusz Okonski <darek200180@gmail.com>
2022-01-18 14:55:27 +0000

    getAverage method

--- src/main/java/Zajavka/week_03/_13/Main.java
+++ src/main/java/Zajavka/week_03/_13/Main.java
@@ -6,11 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = {1,2,3,2,3,4,5,3};
+        int[] array = {1,2,3,2,3,11,4,5,3};
         System.out.println(Arrays.toString(array));
 
-        printArrayElements(array);
+        System.out.println("max: " + getMaxElement(array));
+        System.out.println("avg: " + getAverage(array));
+    }
 
+    private static double getAverage(int[] array) {
+        double total = 0.0;
+        for (int element : array) {
+            total += element;
+        }
+        return (total / array.length);
+    }
+
+    private static int getMaxElement(int[] array) {
+        int max = array[0];
+
+        for (int element : array) {
+            if(element > max)
+                max = element;
+        }
+
+        return max;
     }
 
     private static void printArrayElements(int[] array) {
