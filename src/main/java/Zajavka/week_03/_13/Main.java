5f8c10fcfcf8fdbaa006a084f4cdb3aa0e753175
Dariusz Okonski <darek200180@gmail.com>
2022-01-18 14:22:03 +0000

    working with java

--- src/main/java/Zajavka/week_03/_13/Main.java
+++ src/main/java/Zajavka/week_03/_13/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = new int[10];
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
 
     }
 }
