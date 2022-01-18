ebd643484364dc7e90f4975170d50151dd5959b1
Dariusz Okonski <darek200180@gmail.com>
2022-01-18 14:40:40 +0000

    worknig with arrays

--- src/main/java/Zajavka/week_03/_13/Main.java
+++ src/main/java/Zajavka/week_03/_13/Main.java
@@ -7,10 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         int[] array = {1,2,3,2,3,4,5,3};
-
         System.out.println(Arrays.toString(array));
 
-        int[] array2 = new int[5];
+        printArrayElements(array);
 
+    }
+
+    private static void printArrayElements(int[] array) {
+        System.out.print("[");
+
+        for (int i = 0; i < array.length ; i++) {
+            if(i == array.length - 1) {
+                System.out.print(array[array.length - 1]);
+                break;
+            }
+            System.out.print(array[i] + ", ");
+        }
+        System.out.print("]");
     }
 }
