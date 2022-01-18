2d939657d4d178e5646aa78fd5d64e95fce28d6e
Dariusz Okonski <darek200180@gmail.com>
2022-01-18 14:28:14 +0000

    arrays

--- src/main/java/Zajavka/week_03/_13/Main.java
+++ src/main/java/Zajavka/week_03/_13/Main.java
@@ -1,10 +1,16 @@
 package Zajavka.week_03._13;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int[] array = {1,2,3,2,3,4,5,3};
 
+        System.out.println(Arrays.toString(array));
+
+        int[] array2 = new int[5];
 
     }
 }
