93f4c863f5f5409c591c1d248dd4d95a590103c7
Dariusz Okonski <darek200180@gmail.com>
2022-01-19 13:18:14 +0000

    working with double dimension array

--- src/main/java/Zajavka/week_03/_14/Main.java
+++ src/main/java/Zajavka/week_03/_14/Main.java
@@ -5,18 +5,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[][] array = new int[3][3];
+        int[][] dim2 = {
+                {1, 2, 3},
+                {1, 2, 7},
+                {4, 5, 6}
+        };
+
+        System.out.println(Arrays.deepToString(dim2));
+
+        int[][] array = new int[3][];
         System.out.println(Arrays.deepToString(array));
-
-
-        String[][] strArray = new String[3][];
-        strArray[0] = new String[2];
-        strArray[1] = new String[3];
-        strArray[2] = new String[4];
-
-        System.out.println(Arrays.deepToString(strArray));
-
-
-
     }
 }
