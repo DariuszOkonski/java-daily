9b9128d6d2276b1affedf1cd682877eececf2bfd
Dariusz Okonski <darek200180@gmail.com>
2022-01-20 12:52:35 +0000

    binary search

--- src/main/java/Zajavka/week_03/_15/Main.java
+++ src/main/java/Zajavka/week_03/_15/Main.java
@@ -1,7 +1,15 @@
 package Zajavka.week_03._15;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int[] intArray = {3,1,2,5,2,6,7,9,3,5,6,2,5,6,8};
+        System.out.println(Arrays.toString(intArray));
+        Arrays.sort(intArray);
+        System.out.println(Arrays.toString(intArray));
+
+        System.out.println(Arrays.binarySearch(intArray, 7));
+        System.out.println(Arrays.binarySearch(intArray, 4));
     }
 }
