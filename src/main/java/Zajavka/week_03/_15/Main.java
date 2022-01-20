d16debaefd050a3c85d70344c38178f831e396a0
Dariusz Okonski <darek200180@gmail.com>
2022-01-20 13:02:52 +0000

    bubble sorting

--- src/main/java/Zajavka/week_03/_15/Main.java
+++ src/main/java/Zajavka/week_03/_15/Main.java
@@ -6,10 +6,28 @@ public class Main {
     public static void main(String[] args) {
         int[] intArray = {3,1,2,5,2,6,7,9,3,5,6,2,5,6,8};
         System.out.println(Arrays.toString(intArray));
-        Arrays.sort(intArray);
+        bubbleSort(intArray);
         System.out.println(Arrays.toString(intArray));
+    }
 
-        System.out.println(Arrays.binarySearch(intArray, 7));
-        System.out.println(Arrays.binarySearch(intArray, 4));
+    private static void bubbleSort(int[] intArray) {
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+            sorted = swap(intArray, sorted);
+        }
+    }
+
+    private static boolean swap(int[] intArray, boolean sorted) {
+        for (int i = 0; i < intArray.length - 1; i++) {
+            if(intArray[i] > intArray[i + 1]) {
+                int temp = intArray[i];
+                intArray[i] = intArray[i + 1];
+                intArray[i + 1] = temp;
+                sorted = false;
+            }
+        }
+        return sorted;
     }
 }
