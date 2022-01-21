e0fc5781140e4cb21eeaf85de8b5b74c626a4bf6
Dariusz Okonski <darek200180@gmail.com>
2022-01-21 13:25:36 +0000

    add printIndexOfArraysElem

--- src/main/java/Zajavka/week_03/_15/App.java
+++ src/main/java/Zajavka/week_03/_15/App.java
@@ -1,7 +1,23 @@
 package Zajavka.week_03._15;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        int[] array = {3,2,5,1};
+        printIndexOfArraysElem(array, 5);
+        printIndexOfArraysElem(array, 15);
+    }
+
+    private static void printIndexOfArraysElem(int[] array, int element) {
+        for (int i = 0; i < array.length ; i++) {
+            if(array[i] == element){
+                System.out.println(i);
+                return;
+            }
+        }
+
+        System.out.println("-1");
     }
 }
