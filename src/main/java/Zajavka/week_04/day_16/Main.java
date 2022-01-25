8a089dcd076a70f8222a31139df2de81c3b3cb63
Dariusz Okonski <darek200180@gmail.com>
2022-01-25 23:14:24 +0000

    change Tuna to Salmon

--- src/main/java/Zajavka/week_04/day_16/Main.java
+++ src/main/java/Zajavka/week_04/day_16/Main.java
@@ -3,15 +3,8 @@ package Zajavka.week_04.day_16;
 public class Main {
     public static void main(String[] args) {
 
-        Tuna tuna1 = new Tuna();
-        Tuna tuna2 = new Tuna();
-        Tuna tuna3 = tuna2;
-
-        tuna2.name = "Azor";
-
-        tuna1.swim();
-        tuna2.swim();
-        tuna3.swim();
+        Salmon salmon1 = new Salmon();
+        salmon1.swim();
 
     }
 }
---
+++ src/main/java/Zajavka/week_04/day_16/Salmon.java
@@ -0,0 +1,12 @@
+package Zajavka.week_04.day_16;
+
+public class Salmon {
+    public String surname;
+    public int age;
+    public Integer length;
+
+    public void swim() {
+        String color = "kolor";
+        System.out.println("Swimming: " + this.surname + " age: " + this.age + " length: " + this.length + " color: " + color);
+    }
+}
