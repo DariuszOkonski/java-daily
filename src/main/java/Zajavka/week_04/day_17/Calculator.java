f71e1dc5459e8e36b305c6bc8eddd9291bdadb0e
Dariusz Okonski <darek200180@gmail.com>
2022-01-27 19:31:22 +0000

    equals and hash

--- src/main/java/Zajavka/week_04/day_17/Calculator.java
+++ src/main/java/Zajavka/week_04/day_17/Calculator.java
@@ -1,16 +1,32 @@
 package Zajavka.week_04.day_17;
 
+import java.util.Objects;
+
 public class Calculator {
 
-    public void show(byte b) {
-        System.out.println("byte: " + b);
+    public String name;
+
+    public Calculator(String model) {
+        this.name = model;
     }
 
-    public void show(short s) {
-        System.out.println("short: " + s);
+    public void calculate(String model) {
+        System.out.println(this.name);
     }
 
-    public void show(int i) {
-        System.out.println("int: " + i);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Calculator that = (Calculator) o;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/main/java/Zajavka/week_04/day_17/Main.java
+++ src/main/java/Zajavka/week_04/day_17/Main.java
@@ -2,6 +2,11 @@ package Zajavka.week_04.day_17;
 
 public class Main {
     public static void main(String[] args) {
+        Calculator calc1 = new Calculator("Sony");
+        Calculator calc2 = new Calculator("Sony");
+
+        System.out.println(calc1 == calc2);
+        System.out.println(calc1.equals(calc2));
 
     }
 }
