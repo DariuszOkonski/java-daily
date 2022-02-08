9e67b89d7472ef4d3f2f382fb3903550116b3d8c
Dariusz Okonski <darek200180@gmail.com>
2022-02-08 13:59:23 +0000

    start day 21

--- src/main/java/Zajavka/week_04/day_20/animal/Cat.java
+++ src/main/java/Zajavka/week_04/day_20/animal/Cat.java
@@ -3,13 +3,11 @@ package Zajavka.week_04.day_20.animal;
 import Zajavka.week_04.day_20.animal2.Animal;
 
 public class Cat extends Animal {
-
     private String name = "tasza";
 
     {
         this.name = "szasz";
     }
-
 
     public Cat() {
     }
---
+++ src/main/java/Zajavka/week_05/day_21/Main.java
@@ -0,0 +1,7 @@
+package Zajavka.week_05.day_21;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("day 21");
+    }
+}
