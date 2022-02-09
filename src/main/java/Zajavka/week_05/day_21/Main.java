82a9020c07ec3d9ef0cf65eed3aa452dff01f4f8
Dariusz Okonski <darek200180@gmail.com>
2022-02-09 13:07:30 +0000

    working with abstract classes

--- src/main/java/Zajavka/week_05/day_21/Main.java
+++ src/main/java/Zajavka/week_05/day_21/Main.java
@@ -2,6 +2,9 @@ package Zajavka.week_05.day_21;
 
 public class Main {
     public static void main(String[] args) {
-            // 25min
+
+        Animal animal = new Persian("Antek");
+        animal.getName();
+
     }
 }
