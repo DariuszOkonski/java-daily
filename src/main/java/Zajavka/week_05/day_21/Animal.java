e19dfa035515b2f87557916c46cdaf47dcbff43b
Dariusz Okonski <darek200180@gmail.com>
2022-02-09 13:31:33 +0000

    add getters and setters

--- src/main/java/Zajavka/week_05/day_21/Animal.java
+++ src/main/java/Zajavka/week_05/day_21/Animal.java
@@ -1,15 +1,18 @@
 package Zajavka.week_05.day_21;
 
 public abstract class Animal {
+
     private String name;
 
     public Animal(String name) {
         this.name = name;
     }
 
-    public String getAnything() {
-        return "anything";
+    public String getName() {
+        return name;
     }
 
-    public abstract String getName();
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/Zajavka/week_05/day_21/Main.java
+++ src/main/java/Zajavka/week_05/day_21/Main.java
@@ -1,10 +1,19 @@
 package Zajavka.week_05.day_21;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
         Animal animal = new Persian("Antek");
-        animal.getName();
+        animal.setName("Hi");
 
+        String sth = "someString";
+
+        String result = sth.substring(1)
+                .toUpperCase(Locale.ROOT)
+                .toUpperCase(Locale.ROOT)
+                .trim()
+                .repeat(2);
     }
 }
