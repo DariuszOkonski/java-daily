540f46511dd739b1dce19295edecbb554c07dc14
Dariusz Okonski <darek200180@gmail.com>
2022-02-11 13:53:32 +0000

    change main

--- src/main/java/Zajavka/week_05/day_22a/Main.java
+++ src/main/java/Zajavka/week_05/day_22a/Main.java
@@ -3,19 +3,11 @@ package Zajavka.week_05.day_22a;
 import java.util.ArrayList;
 import java.util.List;
 
+//36min
+
 public class Main {
     public static void main(String[] args) {
-        List<Animal> animals = new ArrayList<>();
-        animals.add(new Cat());
-        animals.add(new Ostrich());
 
-        for (Animal animal: animals) {
-            animal.live();
-        }
-
-        Ostrich ostrich = new Ostrich();
-        boolean isMarkerable = ostrich instanceof Markerable;
-        System.out.println(isMarkerable);
     }
 
     public static void runAnimal(Animal animal) {
