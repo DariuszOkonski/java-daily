1f06a7479f1b2839135b0fc3b61a0ef8345b815a
Dariusz Okonski <darek200180@gmail.com>
2022-02-15 14:25:07 +0000

    add cat

--- src/main/java/Zajavka/week_05/day_23/Main.java
+++ src/main/java/Zajavka/week_05/day_23/Main.java
@@ -1,7 +1,24 @@
 package Zajavka.week_05.day_23;
 
 public class Main {
+//    20min
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Cat cat = new Cat("Andrzej");
+
+        System.out.println(cat);
+//        passByReferenceOrPassByValue(cat);
+        byNewObject(cat);
+
+        System.out.println(cat);
+    }
+
+    private static void passByReferenceOrPassByValue(Cat cat) {
+        cat.setName("Marcin");
+        System.out.println(cat);
+    }
+
+    private static void byNewObject(Cat cat) {
+        cat = new Cat("Piotr");
+        System.out.println(cat);
     }
 }
