bf9239a96ea425ed314bf0d00e7cf9ac68e60aa0
Dariusz Okonski <darek200180@gmail.com>
2022-03-09 17:09:39 +0000

    working with enum

--- src/main/java/Zajavka/week_06/day_26/summary/Animal.java
+++ src/main/java/Zajavka/week_06/day_26/summary/Animal.java
@@ -1,17 +1,44 @@
 package Zajavka.week_06.day_26.summary;
 
 public enum Animal implements Voicable {
-    REINDEER("How how"),
-    SHARK("Shhhr"),
-    CROCODILE("Cwap"),
-    FROG("Rybyt"),
-    GIRAFFE("<eating_leaves>");
+    REINDEER("How how") {
+        @Override
+        public String sing() {
+            return "hi";
+        }
+    },
+    SHARK("Shhhr") {
+        @Override
+        public String sing() {
+            return "hi";
+        }
+    },
+    CROCODILE("Cwap") {
+        @Override
+        public String sing() {
+            return "hi";
+        }
+    },
+    FROG("Rybyt") {
+        @Override
+        public String sing() {
+            return "hi";
+        }
+    },
+    GIRAFFE("<eating_leaves>") {
+        @Override
+        public String sing() {
+            return "hi";
+        }
+    };
 
     private final String message;
 
     Animal(String message) {
         this.message = message;
     }
+
+    public abstract String sing();
 
     public String getMessage() {
         return this.message;
