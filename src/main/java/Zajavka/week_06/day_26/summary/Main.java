0f0438cc01c062fb4e0cd7e06c8f3f578e5f9a5c
Dariusz Okonski <darek200180@gmail.com>
2022-03-09 16:31:55 +0000

    working with enums

--- src/main/java/Zajavka/week_06/day_26/summary/Main.java
+++ src/main/java/Zajavka/week_06/day_26/summary/Main.java
@@ -2,8 +2,30 @@ package Zajavka.week_06.day_26.summary;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
 
+        Animal animal = Animal.GIRAFFE;
 
+        switch (animal) {
+            case SHARK:
+                System.out.println("Ok, its a shark");
+                break;
+            case GIRAFFE:
+                System.out.println("I am a giraffe");
+                break;
+            case FROG:
+            case REINDEER:
+            case CROCODILE:
+                System.out.println("We are crazy animals");
+                break;
+            default:
+                System.out.println("Don't have such an option");
+                break;
+        }
+
+        if(Animal.SHARK.equals(animal)) {
+
+        } else {
+
+        }
     }
 }
