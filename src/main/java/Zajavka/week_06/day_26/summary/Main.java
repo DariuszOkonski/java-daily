e3b19605949f3029a9c62be295c647f429da0774
Dariusz Okonski <darek200180@gmail.com>
2022-03-09 17:03:23 +0000

    add for in main

--- src/main/java/Zajavka/week_06/day_26/summary/Main.java
+++ src/main/java/Zajavka/week_06/day_26/summary/Main.java
@@ -1,18 +1,13 @@
 package Zajavka.week_06.day_26.summary;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
-        int resolvedEnumIndex = 2;
-
-        int someOrdinal = Animal.CROCODILE.ordinal();
-
-        System.out.println(someOrdinal);
+        for (Voicable voicable : Animal.values()) {
+            System.out.println(voicable.makeVoice());
+        }
 
 
-        Animal animal = Animal.values()[someOrdinal];
-        System.out.println(animal);
+
     }
 }
