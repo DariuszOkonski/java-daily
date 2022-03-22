c49c26de25289d4c07164166c505b318c3c4414b
Dariusz Okonski <darek200180@gmail.com>
2022-03-22 15:39:32 +0000

    working with House

--- src/main/java/Zajavka/week_06/day_27/Main.java
+++ src/main/java/Zajavka/week_06/day_27/Main.java
@@ -3,5 +3,11 @@ package Zajavka.week_06.day_27;
 public class Main {
     public static void main(String[] args) {
 
+        HouseBuilder houseBuilder = new HouseBuilder();
+        House house = houseBuilder.buildHouse(5.0, "Zajavkowa 7/5, Tarnowo Podgorne");
+
+        System.out.println(house.getArea());
+        System.out.println(house.getAddress());
+
     }
 }
