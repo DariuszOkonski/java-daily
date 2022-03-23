a53194e2f98095fb3a130e724aa48330ad887b46
Dariusz Okonski <darek200180@gmail.com>
2022-03-23 11:13:59 +0000

    start working at day 28

--- src/main/java/Zajavka/week_06/day_28/Main.java
+++ src/main/java/Zajavka/week_06/day_28/Main.java
@@ -2,6 +2,6 @@ package Zajavka.week_06.day_28;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello111");
+        System.out.println("Hello");
     }
 }
