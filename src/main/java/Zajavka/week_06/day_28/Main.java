8fa7d8cf33029d65bdad9943cc57f5b5a9d87e72
Dariusz Okonski <darek200180@gmail.com>
2022-03-24 12:59:38 +0000

    working with statics

--- src/main/java/Zajavka/week_06/day_28/Main.java
+++ src/main/java/Zajavka/week_06/day_28/Main.java
@@ -1,7 +1,13 @@
 package Zajavka.week_06.day_28;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        printMe();
+    }
+
+    public static void printMe() {
+        System.out.println("Printing me");
     }
 }
