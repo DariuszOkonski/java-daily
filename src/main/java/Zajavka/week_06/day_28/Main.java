54e9b47d16cbcd30835704a6e4b74307bb961a9c
Dariusz Okonski <darek200180@gmail.com>
2022-03-28 13:33:35 +0100

    working at day 28

--- src/main/java/Zajavka/week_06/day_28/Main.java
+++ src/main/java/Zajavka/week_06/day_28/Main.java
@@ -1,14 +1,14 @@
 package Zajavka.week_06.day_28;
 
+import java.math.BigDecimal;
+import java.util.function.Supplier;
+
 public class Main {
 
     public static String name;
 
     public static void main(String[] args) {
 
-        double area = Calculator.calculateCircleArea(10);
-
-        System.out.println("Area is: " + area);
 
 
     }
