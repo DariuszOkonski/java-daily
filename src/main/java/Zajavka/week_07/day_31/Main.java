b6e6b6a5e8c56ef0841133551e5fb9fda863bd80
Dariusz Okonski <darek200180@gmail.com>
2022-03-30 14:09:45 +0100

    working with exceptions

--- src/main/java/Zajavka/week_07/day_31/Main.java
+++ src/main/java/Zajavka/week_07/day_31/Main.java
@@ -1,7 +1,28 @@
 package Zajavka.week_07.day_31;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world111");
+    public static void main(String[] args)  {
+
+        try {
+            printSomething("throw");
+        } catch (Exception e) {
+            System.out.println("Exception was thrown: " + e.getMessage());
+        }
+
+        try {
+            printSomething("something");
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void printSomething(final String input) throws MyException {
+        if("Throw".equalsIgnoreCase(input)) {
+            throw new MyException();
+        } else {
+            System.out.println(input);
+        }
     }
 }
---
+++ src/main/java/Zajavka/week_07/day_31/MyException.java
@@ -0,0 +1,7 @@
+package Zajavka.week_07.day_31;
+
+public class MyException extends Exception {
+    public MyException() {
+        super("My exception was thrown");
+    }
+}
