3e4184f9dfc56c16de32a2d8ee8606d0864c12af
Dariusz Okonski <darek200180@gmail.com>
2022-03-30 12:53:18 +0100

    add emum Order

---
+++ src/main/java/Zajavka/week_06/day_30/Order.java
@@ -0,0 +1,8 @@
+package Zajavka.week_06.day_30;
+
+public enum Order {
+    CREATED,
+    IN_PROGRESS,
+    COMPLEATED,
+    CANCELLED
+}
--- src/main/java/Zajavka/week_06/day_30/SomeEnum.java
+++
@@ -1,5 +0,0 @@
-package Zajavka.week_06.day_30;
-
-public enum SomeEnum {
-
-}
