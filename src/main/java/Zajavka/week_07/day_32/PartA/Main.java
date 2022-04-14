5b94f59c1e31d78258a90d326745c91cf636b3f0
Dariusz Okonski <darek200180@gmail.com>
2022-04-14 12:10:29 +0100

    working with day 32

--- src/main/java/Zajavka/week_07/day_32/PartA/Main.java
+++ src/main/java/Zajavka/week_07/day_32/PartA/Main.java
@@ -2,6 +2,6 @@ package Zajavka.week_07.day_32.PartA;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Day 32");
+        System.out.println("Days 32");
     }
 }
