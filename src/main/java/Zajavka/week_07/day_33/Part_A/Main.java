4a362f5f8a55f0f415223ef000bd1e2a66f432bf
Dariusz Okonski <darek200180@gmail.com>
2022-04-14 13:00:46 +0100

    working with exceptions

--- src/main/java/Zajavka/week_07/day_33/Part_A/Main.java
+++ src/main/java/Zajavka/week_07/day_33/Part_A/Main.java
@@ -1,7 +1,19 @@
 package Zajavka.week_07.day_33.Part_A;
 
 public class Main {
+    private long parameter;
+
+    public Main(long parameter) {
+        if(parameter > 0 && parameter < 10) {
+            this.parameter = parameter;
+        } else {
+            throw new RuntimeException(("what is this dude?"));
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Day 33");
+
+        Main main = new Main(10);
+
     }
 }
