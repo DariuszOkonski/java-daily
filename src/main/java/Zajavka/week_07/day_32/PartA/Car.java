cabc51ce1cbab79781e1fa63e862387dd983c1e4
Dariusz Okonski <darek200180@gmail.com>
2022-04-14 12:17:49 +0100

    add Vehicle and Car

---
+++ src/main/java/Zajavka/week_07/day_32/PartA/Car.java
@@ -0,0 +1,9 @@
+package Zajavka.week_07.day_32.PartA;
+
+public class Car extends Vehicle{
+
+    @Override
+    public void drive() throws Exception {
+        throw new Exception("hello");
+    }
+}
---
+++ src/main/java/Zajavka/week_07/day_32/PartA/Vehicle.java
@@ -0,0 +1,5 @@
+package Zajavka.week_07.day_32.PartA;
+
+public abstract class Vehicle {
+    public abstract void drive() throws Exception;
+}
