780fdc1d706222a7fd25b92311fb07b6e7220225
Dariusz Okonski <darek200180@gmail.com>
2022-04-27 14:45:39 +0100

    working at day 34

--- src/main/java/Zajavka/week_07/day_34/partA/Main.java
+++ src/main/java/Zajavka/week_07/day_34/partA/Main.java
@@ -3,7 +3,5 @@ package Zajavka.week_07.day_34.partA;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
     }
 }
