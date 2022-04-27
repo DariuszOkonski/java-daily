5a2673392ee701e76bcd1b038962872d7ba202b7
Dariusz Okonski <darek200180@gmail.com>
2022-04-27 15:53:09 +0100

    working with collections

--- src/main/java/Zajavka/week_08/day_35/Main.java
+++ src/main/java/Zajavka/week_08/day_35/Main.java
@@ -1,7 +1,21 @@
 package Zajavka.week_08.day_35;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Vector;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Day 35");
+
+        Hashtable hashtable = new Hashtable();
+        hashtable.put(1, "darek");
+        hashtable.put(2, "marcin");
+
+        System.out.println(Collections.unmodifiableMap(hashtable));
+
+
+        Object one = hashtable.get(1);
+        System.out.println(one);
     }
 }
