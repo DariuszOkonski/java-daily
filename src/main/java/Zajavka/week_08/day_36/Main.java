6eecc994fecb6ddef1609a154044165ec4d5dd40
Dariusz Okonski <darek200180@gmail.com>
2022-04-28 15:33:07 +0100

    working with arrayList

--- src/main/java/Zajavka/week_08/day_36/Main.java
+++ src/main/java/Zajavka/week_08/day_36/Main.java
@@ -1,7 +1,21 @@
 package Zajavka.week_08.day_36;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Day 36");
+        List<String> citiesList = new ArrayList<>();
+
+        citiesList.add("Warsaw");
+        citiesList.add("Gdansk");
+        citiesList.add("Wroclaw");
+        System.out.println(citiesList);
+
+        citiesList.add(1, "SZCZECIN");
+        System.out.println(citiesList);
+
+        System.out.println(citiesList.get(0));
     }
 }
