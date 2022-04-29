fa59f563027ee327c705f1b4542249d1579042d4
Dariusz Okonski <darek200180@gmail.com>
2022-04-29 12:19:14 +0100

    working with List

--- src/main/java/Zajavka/week_08/day_36/Main.java
+++ src/main/java/Zajavka/week_08/day_36/Main.java
@@ -1,11 +1,24 @@
 package Zajavka.week_08.day_36;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
+        List<String> listOfStrings = new ArrayList<>();
+        listOfStrings.add("darek");
+        listOfStrings.add("marek");
+        listOfStrings.add("jagoda");
+
+        List<String> someList = new ArrayList<>();
+        someList.add("1");
+        someList.add("2");
+
+        listOfStrings.addAll(someList);
 
 
     }
