5cb19d93c6651bbe5e9ea85da3cbda271b91eb99
Dariusz Okonski <darek200180@gmail.com>
2022-04-29 11:31:08 +0100

    start day 36

--- src/main/java/Zajavka/week_08/day_36/Main.java
+++ src/main/java/Zajavka/week_08/day_36/Main.java
@@ -1,21 +1,12 @@
 package Zajavka.week_08.day_36;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> citiesList = new ArrayList<>();
 
-        citiesList.add("Warsaw");
-        citiesList.add("Gdansk");
-        citiesList.add("Wroclaw");
-        System.out.println(citiesList);
 
-        citiesList.add(1, "SZCZECIN");
-        System.out.println(citiesList);
 
-        System.out.println(citiesList.get(0));
     }
 }
