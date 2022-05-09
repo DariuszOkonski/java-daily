a55f3efe231ac44e3c20ba5017f6ccb32583ab0b
Dariusz Okonski <darek200180@gmail.com>
2022-05-09 13:31:41 +0100

    working with Set

--- src/main/java/Zajavka/week_08/day_38/Main.java
+++ src/main/java/Zajavka/week_08/day_38/Main.java
@@ -1,7 +1,71 @@
 package Zajavka.week_08.day_38;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("start day 38");
+
+        Set<String> set1 = new HashSet<>();
+        set1.add("Gdansk");
+        set1.add("Warszawa");
+        set1.add("Szczecin");
+        set1.add("Szczecin");
+        set1.add("Gdansk");
+        set1.add("Gliwice");
+
+        Set<String> set2 = new HashSet<>();
+        set2.add("Lodz");
+        set2.add("Warszawa");
+        set2.add("Bialystok");
+        set2.add("Szczecin");
+        set2.add("Gdansk");
+        set2.add("Gliwice");
+
+        Set<String> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        System.out.println(union);
+        System.out.println("---");
+
+        Set<String> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println(set1);
+        System.out.println(set2);
+        System.out.println(intersection);
+        System.out.println("---");
+
+        Set<String> difference = new HashSet<>(set2);
+        difference.removeAll(set1);
+        System.out.println(set1);
+        System.out.println(set2);
+        System.out.println(difference);
+
+        System.out.println("---");
+        System.out.println(set1.contains("Warszawa"));
+        System.out.println(set1.contains("Zabrze"));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
