83474ae22ef27eb4844254274f0f8781cbe06d6f
Dariusz Okonski <darek200180@gmail.com>
2022-05-09 12:56:17 +0100

    start day 37

--- src/main/java/Zajavka/week_08/day_37/Main.java
+++ src/main/java/Zajavka/week_08/day_37/Main.java
@@ -2,6 +2,6 @@ package Zajavka.week_08.day_37;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day 37");
+        System.out.println("day 37A");
     }
 }
