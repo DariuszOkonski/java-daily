cce39e60148dc0e28477b605b7b431fe48c88fd1
Dariusz Okonski <darek200180@gmail.com>
2022-05-10 17:24:58 +0100

    working with sets

--- src/main/java/Zajavka/week_08/day_38/Main.java
+++ src/main/java/Zajavka/week_08/day_38/Main.java
@@ -5,7 +5,24 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        Set<Integer> setOfIntegers = new HashSet<>();
+        setOfIntegers.add(8);
+        setOfIntegers.add(10);
+        setOfIntegers.add(25);
+        setOfIntegers.add(7);
+        setOfIntegers.add(11);
 
+        setOfIntegers.remove(10);
+
+        System.out.println(setOfIntegers);
+        System.out.println("---");
+        System.out.println(setOfIntegers.contains(8));
+        System.out.println(setOfIntegers.isEmpty());
+        System.out.println("---");
+
+        for (Integer item: setOfIntegers) {
+            System.out.println(item);
+        }
     }
 }
 
