d04b1267ceb4756275bacc83208f14ac088f2447
Dariusz Okonski <darek200180@gmail.com>
2022-05-12 14:33:18 +0100

    working with Map

--- src/main/java/Zajavka/week_08/day_39/Main.java
+++ src/main/java/Zajavka/week_08/day_39/Main.java
@@ -6,7 +6,19 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Map<String, String> map = new HashMap<>();
+        Map<Integer, String> people = new HashMap<>();
+
+        people.put(1, "Norbert Adamski");
+        people.put(2, "Rafal Bernadynski");
+        people.put(3, "Cecylia Celibat");
+
+        System.out.println(people);
+
+
+        System.out.println("Get key: " + people.get(5));
+        System.out.println("Get key: " + people.get(2));
+
+
 
     }
 }
