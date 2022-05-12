32bdf1be675e0a3d73c363a8a37995c11b6c6a5e
Dariusz Okonski <darek200180@gmail.com>
2022-05-12 14:24:52 +0100

    start worknig on day 39

---
+++ src/main/java/Zajavka/week_08/day_39/Main.java
@@ -0,0 +1,12 @@
+package Zajavka.week_08.day_39;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+
+    }
+}
