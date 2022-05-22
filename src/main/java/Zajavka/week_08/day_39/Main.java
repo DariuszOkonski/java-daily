1bcc924df18ebce083be4eb9a6e9b2b7f26eab8d
Dariusz Okonski <darek200180@gmail.com>
2022-05-22 12:18:37 +0200

    add citiesAttractions

--- src/main/java/Zajavka/week_08/day_39/Main.java
+++ src/main/java/Zajavka/week_08/day_39/Main.java
@@ -1,15 +1,20 @@
 package Zajavka.week_08.day_39;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
-        System.out.println("Ho");
+        Map<String, List<String>> citiesAttractions = new HashMap<>();
 
+        List<String> warsawAttractions = new ArrayList<>();
+        warsawAttractions.add("Palac Kultury i Nauki");
+        warsawAttractions.add("Metro Politechnika");
+        citiesAttractions.put("Warsaw", warsawAttractions);
+
+        citiesAttractions.put("Gdynia", Arrays.asList("Galeria Handlowa Galera", "Tramwaj nr 1"));
+        citiesAttractions.put("London", List.of("London's Eye", "Buckingham Palace"));
+
+        System.out.println(citiesAttractions);
     }
 }
