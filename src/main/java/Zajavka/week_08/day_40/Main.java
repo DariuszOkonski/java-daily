8661942bbe626feb107ed2ddc4f992b3320e80cd
Dariusz Okonski <darek200180@gmail.com>
2022-05-22 13:14:10 +0200

    working with day 40

--- src/main/java/Zajavka/week_08/day_40/Main.java
+++ src/main/java/Zajavka/week_08/day_40/Main.java
@@ -1,9 +1,28 @@
 package Zajavka.week_08.day_40;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+        List<String> listWithCities = new ArrayList<>();
+        listWithCities.add("Warszawa");
+        listWithCities.add("Gdańsk");
+        listWithCities.add("Łódź");
+        listWithCities.add("Wrocław");
 
+        ListIterator<String> stringListIterator = listWithCities.listIterator();
+        while (stringListIterator.hasNext()) {
+            System.out.println(stringListIterator.next());
+        }
+
+        while (stringListIterator.hasPrevious()) {
+            System.out.println(stringListIterator.previous());
+        }
+
+        System.out.println(listWithCities);
+    }
 }
