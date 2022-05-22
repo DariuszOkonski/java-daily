3735697ffeef605877ae67a2668d0f8d4bdb7757
Dariusz Okonski <darek200180@gmail.com>
2022-05-22 12:04:31 +0200

    start working at day 39

--- src/main/java/Zajavka/week_08/day_39/Main.java
+++ src/main/java/Zajavka/week_08/day_39/Main.java
@@ -8,20 +8,7 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-        Map<Integer, String> people = new HashMap<>();
-
-        people.put(1, "Norbert Adamski");
-        people.put(2, "Rafal Bernadynski");
-        people.put(3, "Cecylia Celibat");
-
-        Set<Map.Entry<Integer, String>> entries = people.entrySet();
-        Set<Integer> keysets = people.keySet();
-        Collection<String> values = people.values();
-
-        System.out.println(entries);
-        System.out.println(keysets);
-        System.out.println(values);
-
+        System.out.println("Hello");
 
     }
 }
