23ce1a191f74d2986cbe7e87ac3ac5c875a3c7e7
Dariusz Okonski <darek200180@gmail.com>
2022-05-23 18:15:54 +0200

    start working on day 40

--- src/main/java/Zajavka/week_08/day_40/Main.java
+++ src/main/java/Zajavka/week_08/day_40/Main.java
@@ -8,21 +8,6 @@ import java.util.ListIterator;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> listWithCities = new ArrayList<>();
-        listWithCities.add("Warszawa");
-        listWithCities.add("Gdańsk");
-        listWithCities.add("Łódź");
-        listWithCities.add("Wrocław");
 
-        ListIterator<String> stringListIterator = listWithCities.listIterator();
-        while (stringListIterator.hasNext()) {
-            System.out.println(stringListIterator.next());
-        }
-
-        while (stringListIterator.hasPrevious()) {
-            System.out.println(stringListIterator.previous());
-        }
-
-        System.out.println(listWithCities);
     }
 }
