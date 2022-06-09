c3f743d433f17a7e77e43e30d1048c65c6c8b420
Dariusz Okonski <darek200180@gmail.com>
2022-06-09 18:33:58 +0100

    add recurence

--- src/main/java/Zajavka/week_08/day_41/Main.java
+++ src/main/java/Zajavka/week_08/day_41/Main.java
@@ -3,5 +3,14 @@ package Zajavka.week_08.day_41;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello 41a");
+
+        callMe(1);
+    }
+
+    private static void callMe(int arg) {
+        int number = arg;
+        System.out.println(number);
+        number++;
+        callMe(number);
     }
 }
