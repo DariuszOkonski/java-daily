70df13aad64650ece91f5a1abfecf8239c0735e6
Dariusz Okonski <darek200180@gmail.com>
2022-06-13 18:46:59 +0100

    working with local time

--- src/main/java/Zajavka/week_08/day_42/Main.java
+++ src/main/java/Zajavka/week_08/day_42/Main.java
@@ -1,7 +1,13 @@
 package Zajavka.week_08.day_42;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
     }
 }
