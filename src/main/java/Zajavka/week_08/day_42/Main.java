d1a942135a84ae866ab7ce277558543b5ce7dc68
Dariusz Okonski <darek200180@gmail.com>
2022-06-14 17:59:31 +0100

    working on day 42

--- src/main/java/Zajavka/week_08/day_42/Main.java
+++ src/main/java/Zajavka/week_08/day_42/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println(LocalDate.now());
         System.out.println(LocalTime.now());
         System.out.println(LocalDateTime.now());
+        System.out.println("Hello");
     }
 }
