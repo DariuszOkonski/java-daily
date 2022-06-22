fed09830d050cd41a643c660f5d1a733356b7387
Dariusz Okonski <darek200180@gmail.com>
2022-06-22 17:35:18 +0100

    working with dates

--- src/main/java/Zajavka/week_08/day_42/Main.java
+++ src/main/java/Zajavka/week_08/day_42/Main.java
@@ -3,9 +3,13 @@ package Zajavka.week_08.day_42;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day 42 b");
+        LocalDate localDate = LocalDate.of(2022, Month.JANUARY, 20);
+
+        System.out.println(localDate);
+
     }
 }
