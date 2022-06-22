2883ca1585fe6395081e2e074e00b2d1818d47f5
Dariusz Okonski <darek200180@gmail.com>
2022-06-22 17:59:30 +0100

    working with dates

--- src/main/java/Zajavka/week_08/day_42/Main.java
+++ src/main/java/Zajavka/week_08/day_42/Main.java
@@ -4,12 +4,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate localDate = LocalDate.of(2022, Month.JANUARY, 20);
+        LocalDate localDate = LocalDate.ofYearDay(2020, 130);
+        System.out.println(localDate.minus(5, ChronoUnit.CENTURIES));
 
-        System.out.println(localDate);
-
+        System.out.println(localDate.withMonth(Month.MARCH.ordinal()));
     }
 }
