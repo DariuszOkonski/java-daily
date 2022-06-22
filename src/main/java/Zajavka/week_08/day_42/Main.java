4f15e3c16b343d8d14f11909f224dc41e2ee1bc6
Dariusz Okonski <darek200180@gmail.com>
2022-06-22 17:26:50 +0100

    start day 42

--- src/main/java/Zajavka/week_08/day_42/Main.java
+++ src/main/java/Zajavka/week_08/day_42/Main.java
@@ -6,9 +6,6 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LocalDate.now());
-        System.out.println(LocalTime.now());
-        System.out.println(LocalDateTime.now());
-        System.out.println("Hello");
+        System.out.println("day 42 b");
     }
 }
