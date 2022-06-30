b617a8b8ccf77a941625db961775ee9c17120af4
Dariusz Okonski <darek200180@gmail.com>
2022-06-30 18:18:35 +0100

    start day 45

--- src/main/java/Zajavka/week_08/day_43/Main.java
+++ src/main/java/Zajavka/week_08/day_43/Main.java
@@ -2,6 +2,6 @@ package Zajavka.week_08.day_43;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("start day 44a");
+        System.out.println("start day 45");
     }
 }
